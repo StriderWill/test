daaf35a51e11b927edb770eb009daf538423018a
Claire Williams <itsclairewilliams@gmail.com>
2014-10-24 16:19:04 +0200

    HELP WIHT SSSSSSHSHHHHHSHHSHSHHSHHWS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSsh::Application.config.secret_token = 'a4777b4ee5a87a257f2eaaa6db05459b1fcd87f57db0bd7b35e4918d41a009f8f6e8abd65bb2bd7cfbd11972129c8263d674146dd65c6e710e5bcdda11ced4b8'
